edb479e5836a624e219f1e30cae511e604c52a07
Aasrith Vakulabharanam <aasrithvakul@gmail.com>
2026-09-06 12:23:02 +0530

    Use compatibility build for embedded browsers

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,15 @@
 import { defineConfig } from 'vite';
-export default defineConfig({ base: '/paisapath-finance/' });
+
+export default defineConfig({
+  base: '/paisapath-finance/',
+  build: {
+    rollupOptions: { output: { format: 'iife', inlineDynamicImports: true } },
+  },
+  plugins: [{
+    name: 'classic-browser-script',
+    transformIndexHtml: {
+      order: 'post',
+      handler: (html) => html.replace('type="module" crossorigin', 'defer'),
+    },
+  }],
+});
